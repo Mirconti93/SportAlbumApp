28a92e396a4b59524bf13555e398b0c27570f10e
mircoconti <mircoc@dsgroup.it>
2024-02-19 17:55:57 +0100

    write players

---
+++ app/src/main/java/com/mircontapp/sportalbum/commons/FileDataManager.kt
@@ -0,0 +1,59 @@
+package com.mircontapp.sportalbum.commons
+
+import android.content.Context
+import com.mircontapp.sportalbum.domain.models.PlayerModel
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.OutputStream
+import java.io.OutputStreamWriter
+
+
+class FileDataManager() {
+    companion object {
+        fun writePlayers(context: Context, name: String?, players: List<PlayerModel?>) {
+            val list: MutableList<String> = ArrayList()
+            players.forEach {
+                list.add("${it?.name}_${it?.role}_${it?.gender}_${it?.team}_${it?.country}_${it?.birthyear}_${it?.value}_${it?.valueleg}_${it?.national}_${it?.nationalLegend}_${it?.roleLineUp}_${it?.att}_${it?.dif}_${it?.tec}_${it?.dri}_${it?.fin}_${it?.bal}_${it?.fis}_${it?.vel}_${it?.rig}_${it?.por}")
+            }
+            writeFileOnInternalStorage(context, name, list)
+        }
+
+        /*** write a file on local storage  */
+        private fun writeFileOnInternalStorage(
+            mContext: Context,
+            name: String?,
+            list: List<String?>
+        ): Boolean {
+            val dir = File(mContext.getExternalFilesDir(null), "album_data")
+            if (!dir.exists()) {
+                dir.mkdir()
+            }
+            return try {
+                val file = File(dir, name)
+                if (!file.exists()) {
+                    file.createNewFile()
+                }
+                val writer = FileWriter(file)
+                val bw = BufferedWriter(writer)
+                for (row in list) {
+                    try {
+                        bw.newLine()
+                        bw.write(row)
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
+                }
+                bw.flush()
+                bw.close()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+    }
+}
