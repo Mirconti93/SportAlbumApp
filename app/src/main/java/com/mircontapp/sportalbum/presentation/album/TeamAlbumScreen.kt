9e82c8afb17d20db5039636390e31b6ffb793e84
mircoconti <mircoc@dsgroup.it>
2024-05-07 17:10:35 +0200

    change UI

--- app/src/main/java/com/mircontapp/sportalbum/presentation/album/TeamAlbumScreen.kt
+++ app/src/main/java/com/mircontapp/sportalbum/presentation/album/TeamAlbumScreen.kt
@@ -1,13 +1,22 @@
 package com.mircontapp.sportalbum.presentation.album
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -15,8 +24,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.mircontapp.sportalbum.R
@@ -28,6 +43,7 @@ import com.mircontapp.sportalbum.presentation.commons.OnEditClickHandler
 import com.mircontapp.sportalbum.presentation.commons.OnPlayerClickHandler
 import com.mircontapp.sportalbum.presentation.commons.OnTeamClickHandler
 import com.mircontapp.sportalbum.presentation.navigation.NavigationItem
+import com.mircontapp.sportalbum.presentation.ui.theme.OrangeYellowD
 import com.mircontapp.sportalbum.presentation.viewmodels.MainViewModel
 
 @Composable
@@ -41,11 +57,48 @@ fun TeamAlbumScreen(navController: NavController, mainViewModel: MainViewModel) 
     ) {
         val team = remember{ mainViewModel.teamModel }
         val viewModel: TeamAlbumViewModel = hiltViewModel()
-        val isInited = remember {
+        remember {
             viewModel.playersFromTeamLegend(team)
         }
         viewModel.players.value
-        Text(text = team?.name ?: SportAlbumApplication.instance.getString(R.string.team))
+
+        team?.let {team->
+            Card(
+                modifier = Modifier,
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                ),
+                shape = RoundedCornerShape(4.dp)
+
+            ) {
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+                    Text(modifier = Modifier, text = team.name, fontSize = 20.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
+
+                    val idDrawable = UIHelper.getDrawableId(team.name, R.drawable.empty_logo)
+                    Row(modifier = Modifier
+                        .padding(8.dp, 2.dp)
+                        .fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+                        Image(
+                            painter = painterResource(idDrawable),
+                            contentDescription = "Team icon", // Descrizione opzionale per l'accessibilità
+                            modifier = Modifier.size(70.dp, 70.dp),
+                            contentScale = ContentScale.FillHeight
+                        )
+                        
+                        Spacer(modifier = Modifier.width(20.dp))
+                        Column {
+                            DescriptionText(label = SportAlbumApplication.instance.getString(R.string.city), value = team.city)
+                            DescriptionText(label = SportAlbumApplication.instance.getString(R.string.stadium), value = team.stadium)
+                            DescriptionText(label = SportAlbumApplication.instance.getString(R.string.coach), value = team.coachlegend)
+                        }
+                    }
+                }
+            }
+        }
 
         viewModel.players.value.let {players ->
             PlayersGrid(
@@ -63,11 +116,21 @@ fun TeamAlbumScreen(navController: NavController, mainViewModel: MainViewModel) 
                             navController.navigate(NavigationItem.EditPlayer.route)
                         }
                     },
-
-                    )
+                )
             )
         }
 
+    }
+
+}
+
+@Composable
+fun DescriptionText(label: String, value: String?) {
+    Row {
+        value?.let { text ->
+            Text(modifier = Modifier, text = label + ": ", color = Color.White)
+            Text(modifier = Modifier, text = text, color = OrangeYellowD)
+        }
     }
 
 }
