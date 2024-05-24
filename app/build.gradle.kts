ca2abba4655bc07318f92f517cd627555d9236c5
mircoconti <mircoc@dsgroup.it>
2024-05-24 18:08:22 +0200

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
 
     val nav_version = "2.7.2"
     // Kotlin
@@ -122,6 +123,7 @@ dependencies {
     // Add the dependency for the Realtime Database library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-database")
+
 
 
 }
