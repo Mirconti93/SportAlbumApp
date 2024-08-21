a32e88003a631f959890bb7629650c38cde10bfd
Mirco Conti <mirco.conti@consulenti.mediolanum.it>
2024-08-21 17:03:41 +0200

    edit text

--- app/src/main/java/com/mircontapp/sportalbum/presentation/bupi/BupiScreen.kt
+++ app/src/main/java/com/mircontapp/sportalbum/presentation/bupi/BupiScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -98,15 +99,16 @@ fun BupiScreen(navController: NavController, mainViewModel: MainViewModel) {
 
         Spacer(Modifier.height(8.dp))
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(imageVector = Icons.Filled.Search, contentDescription = "Search icon",
+            Icon(imageVector = Icons.Filled.Create, contentDescription = "Search icon",
                 modifier = Modifier
                     .size(40.dp)
                     .padding(2.dp, 8.dp)
-                    .clickable { showSearch.value = !showSearch.value })
+                    .clickable { showSearch.value = !showSearch.value }.background(Color.White))
             if (showSearch.value) {
                 CustomTextField(
                     value = TextFieldValue(searchUIState.value.searchingText ?: ""),
-                    onValueChange = { newValue -> viewModel.onSearch(newValue.text)}, imageVector = null)
+                    onValueChange = { newValue -> viewModel.onSearch(newValue.text)}
+                    , imageVector = Icons.Filled.Search)
             } else {
                 if (isTeams.value) {
                     Button(onClick = {
--- app/src/main/java/com/mircontapp/sportalbum/presentation/commons/CustomTextField.kt
+++ app/src/main/java/com/mircontapp/sportalbum/presentation/commons/CustomTextField.kt
@@ -1,10 +1,14 @@
 package com.mircontapp.sportalbum.presentation.commons
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -13,56 +17,89 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mircontapp.sportalbum.commons.UIHelper
 import com.mircontapp.sportalbum.commons.customTextEdit
+import com.mircontapp.sportalbum.presentation.ui.theme.BlueD
 import com.mircontapp.sportalbum.presentation.ui.theme.DarkBlueD
 
 @Composable
 fun CustomTextField(value: TextFieldValue,
                     onValueChange: (TextFieldValue) -> Unit, imageVector: ImageVector?) {
-    Card(
-        shape = RoundedCornerShape(4.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-        ),
-    ) {
-        Row(
-            Modifier
-                .height(IntrinsicSize.Min)
-        ) {
-            imageVector?.let {
-                Icon(
-                    imageVector = imageVector,
-                    contentDescription = null,
-                    modifier = Modifier.padding(8.dp)
+
+    Box(modifier = Modifier.padding(8.dp, 4.dp)) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+            cursorBrush = SolidColor(Color.White),
+            decorationBox = { innerTextField ->
+                // Decorate the inner text field with a rounded background and padding
+                Layout(
+                    content = {
+                        Box(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(12.dp))  // Angoli arrotondati
+                                .background(BlueD)  // Sfondo
+                                .padding(16.dp)  // Padding interno
+                                .fillMaxWidth(),
+                            contentAlignment = Alignment.CenterStart
+                        ) {
+                            imageVector?.let {
+                                Icon(
+                                    imageVector = imageVector,
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                        .width(20.dp)
+                                        .height(20.dp)
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(8.dp))
+                            if (value.text.isEmpty()) {
+                                Text(
+                                    text = "Enter text",
+                                    style = TextStyle(color = Color.Gray, fontSize = 16.sp)
+                                )
+                            }
+                            innerTextField()  // Inserisci il campo di testo
+                        }
+                    },
+                    measurePolicy = { measurables, constraints ->
+                        val placeable = measurables.first().measure(constraints)
+                        layout(placeable.width, placeable.height) {
+                            placeable.placeRelative(0, 0)
+                        }
+                    }
                 )
             }
-            BasicTextField(
-                value = value,
-                textStyle = TextStyle(fontSize = 16.sp, color = Color.White),
-                onValueChange = onValueChange,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(58.dp)
-                    .padding(10.dp)
-                    .background(DarkBlueD, shape = RoundedCornerShape(15.dp))
-            )
-
-        }
+        )
     }
+
+
 }
 
 @Composable
 fun CustomTextField(value: TextFieldValue,
                     onValueChange: (TextFieldValue) -> Unit) {
     CustomTextField(value = value, onValueChange = onValueChange, imageVector = null)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomTextFieldPreview() {
+    CustomTextField(TextFieldValue("Ibra"), {})
 }
